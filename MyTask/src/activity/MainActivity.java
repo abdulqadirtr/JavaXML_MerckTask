b786fd09f2b98c6aec2fdd434b3e5074b80d4d40
abdulqadirtr <qadirabdul20@yahoo.com>
2019-12-08 09:34:03 +0100

    * Improved file path: For accessing XML document, File Path is changed from absolute to relative path

--- MyTask/src/activity/MainActivity.java
+++ MyTask/src/activity/MainActivity.java
@@ -1,6 +1,6 @@
 package activity;
 
-
+import java.io.File;
 import java.util.Objects;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -13,8 +13,8 @@ import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
 /**
- * This class read the XML document and retrieve nodes using XPath Parser
- * output Nodes values : a c h
+ * This class read the XML document and retrieve nodes using XPath Parser output
+ * Nodes values : a c h
  * 
  * @author Abdul Qadir
  */
@@ -22,25 +22,29 @@ import org.w3c.dom.NodeList;
 public class MainActivity {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		String fileName = "data/MerkTask_Solve.xml";
+
 		try {
-            
-Document doc = DocumentBuilderFactory.newInstance()
-.newDocumentBuilder()
-.parse("C:\\Users\\ICH\\eclipse-workspace\\MyTask\\src\\data\\MerkTask_Solve.xml");
 
-XPath xpath = XPathFactory.newInstance().newXPath();
+			ClassLoader classLoader = new MainActivity().getClass().getClassLoader();
+			File file = new File(classLoader.getResource(fileName).getFile());
 
-NodeList list = (NodeList) xpath.evaluate("//x1/x2[@id = 'a1']", doc, XPathConstants.NODESET);
-for(int i=0; i < list.getLength(); ++i) {
-Element x2 =  (Element) list.item(i);
-System.out.println("x2 = " + x2.getTextContent());
-}        
-	}
-		catch(Exception e)
-		{
+			Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(file);
+
+			// Creating XPathFactory object
+			XPathFactory xpathFactory = XPathFactory.newInstance();
+			XPath xpath = xpathFactory.newInstance().newXPath();
 			
+			// Retrieve nodes with tag x2 and id = a1, from parent node x1
+			NodeList list = (NodeList) xpath.evaluate("//x1/x2[@id = 'a1']", doc, XPathConstants.NODESET);
+			for (int i = 0; i < list.getLength(); ++i) {
+				Element x2 = (Element) list.item(i);
+				System.out.println("x2 = " + x2.getTextContent());
+			}
+		} catch (Exception e) {
+
 		}
 
-}
+	}
 }
