e2ce4d554aa3c99fc5819077b13546fda4210847
abdulqadirtr <qadirabdul20@yahoo.com>
2019-12-08 18:48:38 +0100

    Try / Catch updated, for handling errors or diagnosing an exceptions i added the following exception ParserConfigurationException SAXException IOException e

--- MyTask/src/activity/MainActivity.java
+++ MyTask/src/activity/MainActivity.java
@@ -1,18 +1,23 @@
 package activity;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Objects;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 /**
+ * 
  * This class read the XML document and retrieve nodes using XPath Parser output
  * Nodes values : a c h
  * 
@@ -21,7 +26,7 @@ import org.w3c.dom.NodeList;
 
 public class MainActivity {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws XPathExpressionException {
 
 		String fileName = "data/MerkTask_Solve.xml";
 
@@ -33,8 +38,7 @@ public class MainActivity {
 			Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(file);
 
 			// Creating XPathFactory object
-			XPathFactory xpathFactory = XPathFactory.newInstance();
-			XPath xpath = xpathFactory.newInstance().newXPath();
+			XPath xpath = XPathFactory.newInstance().newXPath();
 			
 			// Retrieve nodes with tag x2 and id = a1, from parent node x1
 			NodeList list = (NodeList) xpath.evaluate("//x1/x2[@id = 'a1']", doc, XPathConstants.NODESET);
@@ -42,8 +46,8 @@ public class MainActivity {
 				Element x2 = (Element) list.item(i);
 				System.out.println("x2 = " + x2.getTextContent());
 			}
-		} catch (Exception e) {
-
+		} catch (ParserConfigurationException | SAXException | IOException e) {
+			e.printStackTrace();
 		}
 
 	}
